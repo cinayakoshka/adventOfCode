4fe6d08b86d12b1107128ec82214fdc62326dfd9
Bar Shirtcliff <barshirtcliff@gmail.com>
2019-12-23 17:59:30 -0800

    finished day 5 part 2

--- src/test/scala/com/aoc/Day5Test.scala
+++ src/test/scala/com/aoc/Day5Test.scala
@@ -27,14 +27,41 @@ class Day5Test extends AnyFunSpec with Matchers with Utils {
 
   describe("part 2") {
     
-    it("compares things to 8") {
+    it("compares things to 8 - equality") {
       // 3,9,8,9,10,9,4,9,99,-1,8 - Using position mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
       val reader = new IntCodeReader(Seq(3,9,8,9,10,9,4,9,99,-1,8))
       reader.test(8) shouldBe 1
       reader.test(7) shouldBe 0
+    }
+
+    it("compares things to 8 - lt") {
       // 3,9,7,9,10,9,4,9,99,-1,8 - Using position mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
+      val reader = new IntCodeReader(Seq(3,9,7,9,10,9,4,9,99,-1,8))
+      reader.test(7) shouldBe 1
+      reader.test(9) shouldBe 0
+      reader.test(8) shouldBe 0
+    }
+
+    it("compares things to 8 - equality + immediate") {
       // 3,3,1108,-1,8,3,4,3,99 - Using immediate mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
+      val reader = new IntCodeReader(Seq(3,3,1108,-1,8,3,4,3,99))
+      reader.test(8) shouldBe 1
+      reader.test(7) shouldBe 0
+      reader.test(9) shouldBe 0
+    }
+
+    it("compares things to 8 - lt + immediate") {
       // 3,3,1107,-1,8,3,4,3,99 - Using immediate mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
+      val reader = new IntCodeReader(Seq(3,3,1107,-1,8,3,4,3,99))
+      reader.test(7) shouldBe 1
+      reader.test(9) shouldBe 0
+      reader.test(8) shouldBe 0
+    }
+
+    it("the answer") {
+      val source = Source.fromURL(getClass.getResource("/day5Input.txt"))
+      val raw = source.getLines().next().split(",").map(_.toInt)
+      new IntCodeReader(raw).test(5) shouldBe 10428568
     }
   }
 }
